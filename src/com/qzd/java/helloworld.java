d48f1ecb02ebe1042b00cd4da17171e7f2d07559
qzd1226 <690160142@qq.com>
2022-01-01 18:55:44 -0500

    git from idea

---
+++ src/com/qzd/java/helloworld.java
@@ -0,0 +1,9 @@
+package com.qzd.java;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("idea");
+        System.out.println("git from idea");
+    }
+}
